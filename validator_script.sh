3a6991ac50c864c3e885110eaf17ad24c316652e
paranjay <96053895+paranjay-patel@users.noreply.github.com>
2024-08-08 12:19:13 +0530

    Update validator_script.sh

--- validator_script.sh
+++ validator_script.sh
@@ -3,7 +3,7 @@
 # Function to check if PR title contains a JIRA ticket reference (case-insensitive)
 check_jira_ticket_reference() {
     local pr_title="$1"
-    if [[ "$pr_title" =~ ^([A-Za-z]+-[0-9]+):\ (.+)$ ]]; then
+    if [[ "$pr_title" =~ ^([A-Z]+-[0-9]+):\ (.+)$ ]]; then
         jira_ticket="${BASH_REMATCH[1]}"
         title_message="${BASH_REMATCH[2]}"
         jira_ticket_uppercase=$(echo "$jira_ticket" | awk '{print toupper($0)}')
